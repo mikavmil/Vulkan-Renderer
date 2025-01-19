408b054bb5f945e91c906b9fd8590c1cdaf7abac
mikavmil <gamermeganl@gmail.com>
2025-01-20 00:27:06 +0100

    window init

--- Renderer/main.c
+++ Renderer/main.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+const uint32_t WIN_WIDTH = 800;
+const uint32_t WIN_HEIGHT = 600;
+const char* WIN_TITLE = "Vulkan Renderer";
+
+typedef struct App{
+	GLFWwindow* window;
+} App;
+
+void initWindow(App* pApp);
+void initVulkan(App* pApp);
+void mainLoop(App* pApp);
+void cleanup(App* pApp);
+
 int main() {
+	App app = {0};
 
 
-	printf("Hello World\n");
+	initWindow(&app);
+	initVulkan(&app);
+	mainLoop(&app);
+	cleanup(&app);
+
 	return 0;
+}
+
+void initWindow(App *pApp) {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	pApp->window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL);
+}
+
+void initVulkan(App* pApp) {}
+
+void mainLoop(App *pApp) {
+	while (!glfwWindowShouldClose(pApp->window)) {
+		glfwPollEvents();
+	}
+}
+
+void cleanup(App* pApp) {
+	glfwDestroyWindow(pApp->window);
+	glfwTerminate();
 }
